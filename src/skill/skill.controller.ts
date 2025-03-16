b45a125966632e7ccaa599d617e4613e76794e5e
faleialina <august3@mail.ru>
2025-03-16 21:53:34 +0300

    CRUD

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { SkillService } from './skill.service';
 
 @Controller('/skill')
@@ -8,5 +16,25 @@ export class SkillController {
   @Post()
   async create(@Body() obj) {
     return await this.skillService.create(obj);
+  }
+
+  @Get()
+  async getAll() {
+    return await this.skillService.getAllItem();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id') id) {
+    return await this.skillService.getById(id);
+  }
+
+  @Put('/:id')
+  async update(@Param('id') id, @Body() obj) {
+    return await this.skillService.putById(id, obj);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id) {
+    return await this.skillService.deleteById(id);
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('/user')
@@ -8,5 +16,25 @@ export class UserController {
   @Post()
   async create(@Body() obj) {
     return await this.userService.create(obj);
+  }
+
+  @Get()
+  async getAll() {
+    return await this.userService.getAllItem();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id') id) {
+    return await this.userService.getById(id);
+  }
+
+  @Put('/:id')
+  async update(@Param('id') id, @Body() obj) {
+    return await this.userService.putById(id, obj);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id) {
+    return await this.userService.deleteById(id);
   }
 }
